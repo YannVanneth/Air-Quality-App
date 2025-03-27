dbca036ab5003ac9b609b77c4b1236127ca7fe11
Vanneth Yann <yannvanneth.27@gmail.com>
2025-03-27 19:13:13 +0700

    [RE-STRUCTURING] Damm ! Back Dev.

---
+++ backend/src/test/java/com/air_quality_app/rupp/air/quality/app/AirQualityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.air_quality_app.rupp.air.quality.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
