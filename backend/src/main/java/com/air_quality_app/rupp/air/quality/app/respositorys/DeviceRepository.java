e27440ded61431d233b3642ee4788178339f2b18
Vanneth Yann <yannvanneth.27@gmail.com>
2025-04-01 21:36:53 +0700

    [IMPROVING] for automatic exception handling for database operations

--- backend/src/main/java/com/air_quality_app/rupp/air/quality/app/respositorys/DeviceRepository.java
+++ backend/src/main/java/com/air_quality_app/rupp/air/quality/app/respositorys/DeviceRepository.java
@@ -2,7 +2,9 @@ package com.air_quality_app.rupp.air.quality.app.respositorys;
 
 import com.air_quality_app.rupp.air.quality.app.models.DeviceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeviceRepository extends JpaRepository<DeviceModel, Long> {
 
 }
