c6f74598ab96969bd263e3823954cbc1e42945f0
sivkimHENG <hengsivkim14@gmail.com>
2025-04-01 14:10:40 +0700

    test123

---
+++ backend/src/main/java/com/air_quality_app/rupp/air/quality/app/controller/LocationController.java
@@ -0,0 +1,20 @@
+package com.air_quality_app.rupp.air.quality.app.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/location")
+public class LocationController {
+
+
+
+    @Autowired
+    LocationController(){}
+
+
+
+
+
+
+}
